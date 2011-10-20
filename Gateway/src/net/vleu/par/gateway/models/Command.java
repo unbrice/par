3b29a463c474e6737b20aba9c1cad2fe40d4d082
Brice Arnould <unbrice@vleu.net>
2011-10-20 13:11:06 +0200

    Object wrapper for Commands

---
+++ Gateway/src/net/vleu/par/gateway/models/Command.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright ©2011 Brice Arnould
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.vleu.par.gateway.models;
+
+import net.vleu.par.gateway.models.DeviceId.InvalidDeviceIdSerialisation;
+
+public final class Command {
+    private final net.vleu.par.protocolbuffer.Commands.Command.Builder proto;
+
+    public Command(final net.vleu.par.protocolbuffer.Commands.Command proto) {
+        this.proto = proto.toBuilder();
+    }
+    
+    public Command() {
+        this.proto = net.vleu.par.protocolbuffer.Commands.Command.newBuilder();
+    }
+
+    /** @return The DeviceId, null if missing or unparseable */
+    public DeviceId getDeviceId() {
+        if (!this.proto.hasDeviceId())
+            return null;
+        try {
+            return DeviceId.fromProtocolBuffer(this.proto.getDeviceId());
+        }
+        catch (final InvalidDeviceIdSerialisation _) {
+            return null;
+        }
+    }
+}
