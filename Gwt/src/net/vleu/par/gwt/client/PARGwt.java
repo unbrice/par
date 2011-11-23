e2515eafdd31c7c15b43696b4e4beceef0850872
Brice Arnould <unbrice@vleu.net>
2011-11-23 11:13:50 +0100

    GWT: Removed server-side parts

--- Gwt/src/net/vleu/par/gwt/client/PARGwt.java
+++ Gwt/src/net/vleu/par/gwt/client/PARGwt.java
@@ -1,18 +1,12 @@
 package net.vleu.par.gwt.client;
 
-import net.vleu.par.gwt.shared.FieldVerifier;
-import net.vleu.par.protocolbuffer.Devices.DeviceIdData;
-import net.vleu.par.protocolbuffer.GatewayCommands.GatewayRequestData;
-
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
@@ -32,12 +26,6 @@ public class PARGwt implements EntryPoint {
     private static final String SERVER_ERROR = "An error occurred while "
         + "attempting to contact the server. Please check your network "
         + "connection and try again.";
-
-    /**
-     * Create a remote service proxy to talk to the server-side Greeting service.
-     */
-    private final GreetingServiceAsync greetingService = GWT
-            .create(GreetingService.class);
 
     /**
      * This is the entry point method.
@@ -80,14 +68,6 @@ public class PARGwt implements EntryPoint {
         dialogVPanel.add(closeButton);
         dialogBox.setWidget(dialogVPanel);
         
-        
-
-        final Label protoText = new Label();
-        final DeviceIdData.Builder proto = DeviceIdData.newBuilder();
-        proto.setAndroidId(42);
-        protoText.setText(proto.toString());
-        Window.alert(proto.toString());
-        
         // Add a handler to close the DialogBox
         closeButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
@@ -103,7 +83,7 @@ public class PARGwt implements EntryPoint {
              * Fired when the user clicks on the sendButton.
              */
             public void onClick(ClickEvent event) {
-                sendNameToServer();
+                ; //TODO
             }
 
             /**
@@ -111,48 +91,8 @@ public class PARGwt implements EntryPoint {
              */
             public void onKeyUp(KeyUpEvent event) {
                 if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-                    sendNameToServer();
+                    ; // TODO
                 }
-            }
-
-            /**
-             * Send the name from the nameField to the server and wait for a response.
-             */
-            private void sendNameToServer() {
-                // First, we validate the input.
-                errorLabel.setText("");
-                String textToServer = nameField.getText();
-                if (!FieldVerifier.isValidName(textToServer)) {
-                    errorLabel.setText("Please enter at least four characters");
-                    return;
-                }
-
-                // Then, we send the input to the server.
-                sendButton.setEnabled(false);
-                textToServerLabel.setText(textToServer);
-                serverResponseLabel.setText("");
-                greetingService.greetServer(textToServer,
-                        new AsyncCallback<String>() {
-                            public void onFailure(Throwable caught) {
-                                // Show the RPC error message to the user
-                                dialogBox
-                                        .setText("Remote Procedure Call - Failure");
-                                serverResponseLabel
-                                        .addStyleName("serverResponseLabelError");
-                                serverResponseLabel.setHTML(SERVER_ERROR);
-                                dialogBox.center();
-                                closeButton.setFocus(true);
-                            }
-
-                            public void onSuccess(String result) {
-                                dialogBox.setText("Remote Procedure Call");
-                                serverResponseLabel
-                                        .removeStyleName("serverResponseLabelError");
-                                serverResponseLabel.setHTML(result);
-                                dialogBox.center();
-                                closeButton.setFocus(true);
-                            }
-                        });
             }
         }
 
